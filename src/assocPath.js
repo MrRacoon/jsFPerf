e797ac4b476530193b50f36550fe5f69c228a1a6
Jed Clinger <jed.clinger@nike.com>
2017-03-14 20:07:04 -0700

    Add mori to benchmarks

--- src/assocPath.js
+++ src/assocPath.js
@@ -1,15 +1,19 @@
-const I = require('immutable');
-const R = require('ramda');
-const _ = require('lodash/fp');
+const I    = require('immutable');
+const R    = require('ramda');
+const _    = require('lodash/fp');
+const mori = require('mori');
 
 const Benchmark = require('benchmark');
 const suite = new Benchmark.Suite('assocPath');
 
 module.exports = ({ object }) => {
   const { value, path, pathStr } = object;
-  const immVal = I.fromJS(value);
+  const immVal  = I.fromJS(value);
+  const moriVal = mori.toClj(value);
   return suite
     .add('immutable.setIn', () => { immVal.setIn(path, 42); })
+    .add('mori.assocIn', () => { mori.assocIn(moriVal, path, 42); })
     .add('ramda.assocPath', () => { R.assocPath(path, 42, value); })
-    .add('lodash.update', () => { _.update(pathStr, 42,  value); });
+    .add('lodash.set (string path)', () => { _.set(pathStr, 42,  value); })
+    .add('lodash.set (array path)', () => { _.set(path, 42,  value); });
 }
--- src/path.js
+++ src/path.js
@@ -1,16 +1,19 @@
-const I = require('immutable');
-const R = require('ramda');
-const _ = require('lodash/fp');
+const I    = require('immutable');
+const R    = require('ramda');
+const _    = require('lodash/fp');
+const mori = require('mori');
 
 const Benchmark = require('benchmark');
 const suite = new Benchmark.Suite;
 
 module.exports = ({ object }) => {
   const { value, path, pathStr } = object;
-  const immVal = I.fromJS(value);
+  const immVal  = I.fromJS(value);
+  const moriVal = mori.toClj(value);
   return suite
-    .add('immutable.getIn', () => { immVal.getIn(path); })
-    .add('ramda.path'     , () => { R.path(path, value); })
-    .add('lodash.get'     , () => { _.get(pathStr, value); });
-
+    .add('immutable.getIn',          () => { immVal.getIn(path); })
+    .add('mori.getIn',               () => { mori.getIn(moriVal, path); })
+    .add('ramda.path',               () => { R.path(path, value); })
+    .add('lodash.get (string path)', () => { _.get(pathStr, value); })
+    .add('lodash.get (array path)',  () => { _.get(path, value); });
 }
