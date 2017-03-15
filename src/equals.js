2f5a7d62291b8dfc20f7daba8ad652372348bdd2
Jake Nylund <jake.nylund@nike.com>
2017-03-15 10:25:17 -0700

    use object param

--- src/equals.js
+++ src/equals.js
@@ -2,18 +2,20 @@ const R = require('ramda');
 const I = require('immutable');
 const _  = require('lodash');
 
-const first =  { 'a': 1 };
-const second = { 'a': 1 };
-
 const Benchmark = require('benchmark');
 const suite = new Benchmark.Suite('equals');
 
-module.exports = () => {
-  const firstMap = I.Map(first);
-  const secondMap = I.Map(second);
+module.exports = ({ object }) => {
+  const secondObject = Object.assign({}, object);
+  const firstMap = I.Map(object);
+  const secondMap = I.Map(object);
+
+  console.log('lodash', _.isEqual(object, secondObject));
+  console.log('immutable', I.is(firstMap, secondMap));
+  console.log('ramda', R.equals(object, secondObject));
 
   return suite
-    .add('lodash.isEqual', () => { _.isEqual(first, second); })
+    .add('lodash.isEqual', () => { _.isEqual(object, secondObject); })
     .add('immutable.is', () => { I.is(firstMap, secondMap); })
-    .add('ramda.equals', () => { R.equals(first, second); });
+    .add('ramda.equals', () => { R.equals(object, secondObject); });
 }
