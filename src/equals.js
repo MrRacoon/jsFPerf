189a185eba7bbb5ed2b3f3d86f4cdcec54fcb07e
Jake Nylund <jake.nylund@nike.com>
2017-03-15 10:26:18 -0700

    remove console log

--- src/equals.js
+++ src/equals.js
@@ -10,10 +10,6 @@ module.exports = ({ object }) => {
   const firstMap = I.Map(object);
   const secondMap = I.Map(object);
 
-  console.log('lodash', _.isEqual(object, secondObject));
-  console.log('immutable', I.is(firstMap, secondMap));
-  console.log('ramda', R.equals(object, secondObject));
-
   return suite
     .add('lodash.isEqual', () => { _.isEqual(object, secondObject); })
     .add('immutable.is', () => { I.is(firstMap, secondMap); })
